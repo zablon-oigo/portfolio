3a1a0a6dd463d98fcea1f1a139cac20dac99509d
zablon <zablonoigo51@gmail.com>
2024-01-30 15:01:55 +0300

    Style: Increase padding on x-axis

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,13 @@ function Contact() {
         <div name="contact" className="min-h-screen " data-aos='zoom-in-up' data-aos-duration="3000">
             <div className="max-w-4xl mx-auto">
                 <h2 className="text-5xl md:text-6xl text-center font-bold text-[#ccd6f6] underline underline-offset-8 decoration-4 decoration-pink-600">Contact</h2>
-                <div className="flex justify-center flex-col-reverse md:flex-row items-center gap-4 my-10">
+                <p className="mt-6 text-xl font-bold text-center text-[#ccd6f6]">
+          Ready to Craft Your Digital Legacy?
+        </p>
+        <p className="text-xl text-center text-[#ccd6f6]">
+          Let's collaborate to bring your vision to life. Contact me today, and together we'll weave a web that resonates, inspires, and transforms.
+        </p>
+                <div className="flex justify-center flex-col-reverse md:flex-row items-center gap-4 my-10 px-10">
 
                     <form action="https://getform.io/f/a83e15cb-c283-468a-b1ce-cd06ec6139ec" className="w-full md:w-3/4 " method='post'>
                         <label htmlFor="name" className="block text-[#8892b0] text-xl mb-2">Name</label>
